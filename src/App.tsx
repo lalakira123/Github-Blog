f037fb60b0ab9f9340ae58aaf3e85fc73cec54d5
Laerte <laerteakira@gmail.com>
2023-01-03 09:55:24 -0300

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      Hello World
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+
