a04220de529ee33794d648e96d23e12d95e3217c
Laerte <laerteakira@gmail.com>
2023-01-04 15:25:42 -0300

    feat: Issue Card Component

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,6 +1,6 @@
 import { Profile } from "./components/Profile";
 import { SearchIssue } from "./components/SearchIssue";
-import { PostsContainer } from "./styles";
+import { PostsContainer, IssuesContainer, IssueCard, IssueHeader } from "./styles";
 
 
 export function Posts() {
@@ -8,6 +8,55 @@ export function Posts() {
 		<PostsContainer>
 			<Profile />
 			<SearchIssue />
+			<IssuesContainer>
+				<IssueCard>
+					<IssueHeader>
+						<h3>JavaScript data types and data structures</h3>
+						<span>Há 1 dia</span>
+					</IssueHeader>
+					<p>
+					   Programming languages all have built-in data structures, but these 
+					   often differ from one language to another. This article attempts to 
+					   list the built-in data structures available in...
+					</p>
+				</IssueCard>
+
+				<IssueCard>
+					<IssueHeader>
+						<h3>JavaScript data types and data structures</h3>
+						<span>Há 1 dia</span>
+					</IssueHeader>
+					<p>
+					   Programming languages all have built-in data structures, but these 
+					   often differ from one language to another. This article attempts to 
+					   list the built-in data structures available in...
+					</p>
+				</IssueCard>
+
+				<IssueCard>
+					<IssueHeader>
+						<h3>JavaScript data types and data structures</h3>
+						<span>Há 1 dia</span>
+					</IssueHeader>
+					<p>
+					   Programming languages all have built-in data structures, but these 
+					   often differ from one language to another. This article attempts to 
+					   list the built-in data structures available in...
+					</p>
+				</IssueCard>
+
+				<IssueCard>
+					<IssueHeader>
+						<h3>JavaScript data types and data structures</h3>
+						<span>Há 1 dia</span>
+					</IssueHeader>
+					<p>
+					   Programming languages all have built-in data structures, but these 
+					   often differ from one language to another. This article attempts to 
+					   list the built-in data structures available in...
+					</p>
+				</IssueCard>
+			</IssuesContainer>
 		</PostsContainer>
 	)
 }
--- src/pages/Posts/styles.ts
+++ src/pages/Posts/styles.ts
@@ -6,3 +6,43 @@ export const PostsContainer = styled.main`
 	margin: 0 auto;
 	padding: 0 1rem;
 `
+
+export const IssuesContainer = styled.section`
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	gap: 2rem;
+	margin: 3rem 0;
+`
+
+export const IssueCard = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1.25rem;
+	max-width: 26rem;
+	padding: 2rem;
+	background-color: ${(props) => props.theme["base-post"]};
+	border-radius: 10px;
+
+	&:hover {
+		box-shadow: 0 0 0 2px ${(props) => props.theme["base-label"]};
+		cursor: pointer;
+	}
+`
+
+export const IssueHeader = styled.header`
+	display: flex;
+	gap: 1rem;
+
+	h3 {
+		color: ${(props) => props.theme["base-title"]};
+		font-size: 1.25rem;
+		font-weight: 700;
+		padding-right: 2rem;
+	}
+
+	span {
+		color: ${(props) => props.theme["base-span"]};
+		font-size: 0.75rem;
+	}
+`
+
